ca9639c5b682e8b18d0db237283583e1ffa98003
Smehdii <siddik.mehdi@gmail.com>
2019-03-09 18:17:18 +0100

    Setting up Basic Field Component & Some styles stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MaterialModule } from "./material.module";
+import { MaterialModule } from "./material/material.module";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { InputComponent } from "./components/input/input.component";
+import { ButtonComponent } from "./components/button/button.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, InputComponent, ButtonComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
     ReactiveFormsModule,
