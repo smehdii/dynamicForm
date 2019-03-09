02e3411bf1768f749b31bd2209df3c5bcdac4ea6
Smehdii <siddik.mehdi@gmail.com>
2019-03-09 21:44:42 +0100

    dynamic Form Component setup done !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { MaterialModule } from "./material/material.module";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { InputComponent } from "./components/input/input.component";
 import { ButtonComponent } from "./components/button/button.component";
+import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 
 @NgModule({
-  declarations: [AppComponent, InputComponent, ButtonComponent],
+  declarations: [AppComponent, InputComponent, ButtonComponent, DynamicFormComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
