ef15bfe0ed49c2873338ca0765dab9eee37dd02b
Smehdii <siddik.mehdi@gmail.com>
2019-03-10 11:53:12 +0100

    Dynamic Form Done !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { DynamicFormComponent } from "./components/dynamic-form/dynamic-form.component";
+import { FieldConfig } from "./models/field.interface";
+import { Validators } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -6,5 +9,85 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = "dynamicForm";
+  @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
+
+  regConfig: FieldConfig[] = [
+    {
+      type: "input",
+      label: "Username",
+      inputType: "text",
+      name: "name"
+    },
+    {
+      type: "input",
+      label: "Email Address",
+      inputType: "email",
+      name: "email",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Email Required"
+        },
+        {
+          name: "pattern",
+          validator: Validators.pattern(
+            "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$"
+          ),
+          message: "Invalid email"
+        }
+      ]
+    },
+    {
+      type: "input",
+      label: "Password",
+      inputType: "password",
+      name: "password",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Password Required"
+        }
+      ]
+    },
+    {
+      type: "radiobutton",
+      label: "Gender",
+      name: "gender",
+      options: ["Male", "Female"],
+      value: "Male"
+    },
+    {
+      type: "date",
+      label: "DOB",
+      name: "dob",
+      validations: [
+        {
+          name: "required",
+          validator: Validators.required,
+          message: "Date of Birth Required"
+        }
+      ]
+    },
+    {
+      type: "select",
+      label: "Country",
+      name: "country",
+      value: "UK",
+      options: ["India", "UAE", "UK", "US"]
+    },
+    {
+      type: "checkbox",
+      label: "Accept Terms",
+      name: "term",
+      value: true
+    },
+    {
+      type: "button",
+      label: "Save"
+    }
+  ];
+
+  submit(value: any) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MaterialModule } from "./material/material.module";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { InputComponent } from "./components/input/input.component";
 import { ButtonComponent } from "./components/button/button.component";
-import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
+import { SelectComponent } from "./components/select/select.component";
+import { DateComponent } from "./components/date/date.component";
+import { RadiobuttonComponent } from "./components/radiobutton/radiobutton.component";
+import { CheckboxComponent } from "./components/checkbox/checkbox.component";
+import { DynamicFieldDirective } from "./components/dynamic-field/dynamic-field.directive";
+import { DynamicFormComponent } from "./components/dynamic-form/dynamic-form.component";
+import { MaterialModule } from "./material/material.module";
 
 @NgModule({
-  declarations: [AppComponent, InputComponent, ButtonComponent, DynamicFormComponent],
+  declarations: [
+    AppComponent,
+    InputComponent,
+    ButtonComponent,
+    SelectComponent,
+    DateComponent,
+    RadiobuttonComponent,
+    CheckboxComponent,
+    DynamicFieldDirective,
+    DynamicFormComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -19,6 +34,14 @@ import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.com
     FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    InputComponent,
+    ButtonComponent,
+    SelectComponent,
+    DateComponent,
+    RadiobuttonComponent,
+    CheckboxComponent
+  ]
 })
 export class AppModule {}
